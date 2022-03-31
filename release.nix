2021f02759331a1200bee4c037d186242290238b
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2022-03-31 12:26:13 +0200

    Remove the split outputs for now as they currently don't work anyway

--- release.nix
+++ release.nix
@@ -1,28 +1,25 @@
 let
   sources = import ./nix/sources.nix {};
   pkgs = import sources.nixpkgs {};
-  inherit (pkgs) cmake meson ninja pkg-config stdenv;
+  inherit (pkgs) meson ninja pkg-config stdenv;
 in
 
 rec {
   lib = rec {
     a = stdenv.mkDerivation {
       name = "liba";
+      # the pkg-config files will know that they need ac and ad
+      # but they can't refer to the nix store alone as pkg-config would
+      # violate its own interface.
+      # So we propagate this further through the nix layer
       propagatedBuildInputs = [ ac ad ];
       nativeBuildInputs = [ meson ninja pkg-config ];
       src = ./a;
-      outputs = [ "out" "dev" ];
-    };
-
-    b = stdenv.mkDerivation {
-      name = "libb";
-      nativeBuildInputs = [ meson ninja ];
-      src = ./b;
     };
 
     ac = stdenv.mkDerivation {
       name = "libac";
-      nativeBuildInputs = [ meson ninja cmake ];
+      nativeBuildInputs = [ meson ninja ];
       src = ./ac;
     };
 
@@ -31,12 +28,18 @@ rec {
       nativeBuildInputs = [ meson ninja ];
       src = ./ad;
     };
+
+    b = stdenv.mkDerivation {
+      name = "libb";
+      nativeBuildInputs = [ meson ninja ];
+      src = ./b;
+    };
   };
 
   app = stdenv.mkDerivation {
     name = "myapp";
     buildInputs = with lib; [ a b ];
-    nativeBuildInputs = [ pkgs.breakpointHook meson ninja pkg-config ];
+    nativeBuildInputs = [ meson ninja pkg-config ];
     src = ./app;
   };
 }
